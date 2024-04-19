f7baa7583f51ef2317c2516e41bfa6d5bcd8088b
Mahonto <64306095+sabujdev@users.noreply.github.com>
2024-04-19 22:05:36 +0600

    constant time complexcity done

--- app.js
+++ app.js
@@ -9,9 +9,19 @@ function largeElement(arr) {
   return largeEl;
 }
 
-console.log(largeElement([5, 11, 22, 10, 30]));
+console.log(largeElement([5, 11, 22, 10, 30])); //30
 
 // el = 5 run = 5
 // el = 1000 run = 1000
 // el = n run = n
-// 0(n) ---> big o of n ===> linear time complexcity
+// o(n) ---> big o of n ===> linear time complexcity
+
+function findFirstElement(arr) {
+  return arr.at(0);
+}
+console.log(findFirstElement([10, 20, 60]));
+
+// el = 10 run = 1
+// el = 100 run = 1
+// el = n run = 1
+// o(1) ----> constant time complexcity
